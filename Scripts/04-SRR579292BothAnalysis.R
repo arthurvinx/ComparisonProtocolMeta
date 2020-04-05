c1224b3bf806a4b1af00dcdbbd45deff4fa1aca4
Diego Morais <arthur.vinx@gmail.com>
2020-04-05 10:02:01 -0300

    Create 04-SRR579292BothAnalysis.R

---
+++ Scripts/04-SRR579292BothAnalysis.R
@@ -0,0 +1,321 @@
+# Clean global environment ####
+
+rm(list = ls())
+
+# Load libraries ####
+
+library(data.table)
+library(dplyr)
+library(GO.db)
+library(vegan)
+
+# Set directory ####
+
+dir <- "../Data/"
+
+# Taxonomic ####
+
+# Load files and get general information ####
+
+# Protocol results
+
+taxP <- fread(paste0(dir, "SRR579292/protocolLCA.txt"), stringsAsFactors = F, head = F, sep = "\t")
+
+# Assigned
+
+nrow(taxP)-sum(grepl("^Unknown$", taxP$V2))
+
+# Not assigned
+
+sum(grepl("^Unknown$", taxP$V2))
+
+# Grouped by superkingdom
+
+table(sapply(strsplit(taxP$V2, ";"), "[", 1))
+
+# Megan results
+
+taxM <- fread(paste0(dir, "SRR579292/meganLCA.txt"), stringsAsFactors = F, head = F)
+
+# Assigned
+
+unname(nrow(taxM)-table(sapply(strsplit(taxM$V2, ";"), "[", 2))[2])
+
+# Not assigned
+
+table(sapply(strsplit(taxM$V2, ";"), "[", 2))[2]
+
+# Grouped by superkingdom
+
+idx <- grepl(";Viruses;", taxM$V2)
+sum(idx)
+
+idx <- grepl(";Bacteria;", taxM$V2)
+sum(idx)
+
+idx <- grepl(";Archaea;", taxM$V2)
+sum(idx)
+
+idx <- grepl(";Eukaryota;", taxM$V2)
+sum(idx)
+
+# Remove not assigned
+
+taxP <- taxP[!(grepl("^Unknown$", taxP$V2)),]
+taxM <- taxM[!(grepl("^NCBI;Not assigned;$", taxM$V2)),]
+
+# Get only bacteria
+
+taxP <- taxP[grepl("^Bacteria", taxP$V2),]
+taxM <- taxM[grepl("^NCBI;cellular organisms;Bacteria;", taxM$V2),]
+
+# Count taxa
+
+taxP <- as.data.frame(table(taxP$V2), stringsAsFactors = F)
+taxM <- as.data.frame(table(taxM$V2), stringsAsFactors = F)
+
+taxP <- taxP[order(taxP$Freq, decreasing = T),]
+taxM <- taxM[order(taxM$Freq, decreasing = T),]
+rownames(taxP) <- NULL
+rownames(taxM) <- NULL
+
+# View top 10
+
+View(head(taxP, 10))
+View(head(taxM, 10))
+
+# Compare top 10 ####
+
+getCount <- function(desc, M, P){
+  message(desc)
+  message("MEGAN")
+  desc <- gsub("\\[|\\]", ".", desc)
+  idx <- which(grepl(paste0(";", desc, ";$"), M$Var1))
+  message("Position ", idx)
+  message(M[idx,2])
+  message("Protocol")
+  desc <- gsub(" ", "_", desc)
+  if(desc=="Bacteria"){
+    idx <- which(grepl(paste0("^", desc, ";$"), P$Var1))
+  }else{
+    idx <- which(grepl(paste0(";", desc, ";$"), P$Var1))
+  }
+  message("Position ", idx)
+  message(P[idx,2])
+  message("\n")
+}
+
+top10M <- sapply(strsplit(taxM[1:10, 1], ";"), tail, n = 1)
+invisible(sapply(top10M, getCount, M = taxM, P = taxP))
+
+top10P <- sapply(strsplit(taxP[1:10, 1], ";"), tail, n = 1)
+top10P <- gsub("_", " ", top10P)
+invisible(sapply(top10P, getCount, M = taxM, P = taxP))
+
+# Get diversity indices ####
+
+# Protocol
+
+v <- taxP$Freq
+
+H <- diversity(v)
+EH <- H/log(specnumber(v))
+
+round(H, digits = 4)
+round(EH, digits = 4)
+
+# Megan
+
+v <- taxM$Freq
+
+H <- diversity(v)
+EH <- H/log(specnumber(v))
+
+round(H, digits = 4)
+round(EH, digits = 4)
+
+# Compare related to the top 10 ####
+
+getAllRelated <- function(desc, M, P){
+  message(desc)
+  desc <- gsub("\\[|\\]", ".", desc)
+  message("MEGAN")
+  if(desc=="Akkermansia muciniphila"){ # avoiding other strains
+    idx <- which(grepl(paste0(";", desc, ";$"), M$Var1))
+  }else{
+    idx <- which(grepl(paste0(";", desc, ";"), M$Var1))
+  }
+  aux <- M[idx,]
+  message(nrow(aux))
+  message("Total ", sum(aux$Freq))
+  message("Protocol")
+  desc <- gsub(" ", "_", desc)
+  if(desc=="Bacteria"){
+    idx <- which(grepl(paste0("^", desc, ";"), P$Var1))
+  }else{
+    idx <- which(grepl(paste0(";", desc, ";"), P$Var1))
+  }
+  aux <- P[idx,]
+  message(nrow(aux))
+  message("Total ", sum(aux$Freq))
+  message("\n")
+}
+
+top <- unique(c(top10M, top10P))
+invisible(sapply(top, getAllRelated, M = taxM, P = taxP))
+
+# Functional ####
+
+# Load files and get general information ####
+
+# Protocol results
+
+funcP <- as.data.frame(fread(paste0(dir, "SRR579292/protocolFunctional.txt"), stringsAsFactors = F, head = T))
+
+# Not assigned
+
+sum(funcP$Annotation=="Unknown")
+
+# Assigned
+
+nrow(funcP)-sum(funcP$Annotation=="Unknown")
+
+# Remove not assigned
+
+funcP <- funcP[funcP$Annotation!="Unknown", ]
+
+# Megan results
+
+funcM <- as.data.frame(fread(paste0(dir, "SRR579292/meganFunctional.txt"), stringsAsFactors = F, head = F))
+funcM <- funcM[grepl("^GO", funcM$V2),]
+funcM$V2 <- sapply(strsplit(funcM$V2, " "), "[", 1)
+funcM <- funcM %>% group_by(V1) %>% summarise(gos = paste(unique(V2), collapse = "; "))
+funcM <- as.data.frame(funcM)
+
+# Not assigned
+
+257567 - nrow(funcM)
+
+# Assigned
+
+nrow(funcM)
+
+# Count terms
+
+groupedP <- as.data.frame(table(unlist(strsplit(funcP$Annotation, "; "))), stringsAsFactors = F)
+groupedM <- as.data.frame(table(unlist(strsplit(funcM$gos, "; "))), stringsAsFactors = F)
+
+# Get ontology ####
+
+getGOOntology <- function(GOs){
+  BP <- names(as.list(GO.db::GOBPCHILDREN))
+  CC <- names(as.list(GO.db::GOCCCHILDREN))
+  MF <- names(as.list(GO.db::GOMFCHILDREN))
+  result <- vapply(GOs, function(x){
+    if(x %in% BP){
+      return("BP")
+    }else if(x %in% CC){
+      return("CC")
+    }else if(x %in% MF){
+      return("MF")
+    }else{
+      return(NA_character_)
+    }
+  }, character(1))
+  return(result)
+}
+
+groupedP$ont <- getGOOntology(groupedP$Var1)
+groupedM$ont <- getGOOntology(groupedM$Var1)
+
+# Check missing data
+
+which(is.na(groupedP$ont))
+which(is.na(groupedM$ont))
+
+# Get description ####
+
+xx <- as.list(GOTERM)
+getGODescription <- function(i){
+  flagError <- FALSE
+  tryCatch({
+    res <- xx[[i]]@Term
+  }, error = function(e) {
+    flagError <<- TRUE
+  })
+  if(flagError){
+    return("Unknown")
+  }
+  else{
+    return(res)
+  }
+}
+
+groupedP$desc <- sapply(groupedP$Var1, getGODescription)
+groupedM$desc <- sapply(groupedM$Var1, getGODescription)
+
+groupedP$desc[3318] <- "lipoprotein releasing activity"
+groupedP$ont[3318] <- "MF"
+
+groupedP <- groupedP[order(groupedP$Freq, decreasing = T),]
+groupedM <- groupedM[order(groupedM$Freq, decreasing = T),]
+rownames(groupedP) <- NULL
+rownames(groupedM) <- NULL
+
+# Grouped by ontology
+
+sum(groupedP$ont=="BP")
+sum(groupedP$ont=="CC")
+sum(groupedP$ont=="MF")
+
+sum(groupedM$ont=="BP")
+sum(groupedM$ont=="CC")
+sum(groupedM$ont=="MF")
+
+# Number of shared terms
+
+sum(groupedM$Var1[groupedM$ont=="BP"] %in% groupedP$Var1)
+sum(groupedM$Var1[groupedM$ont=="CC"] %in% groupedP$Var1)
+sum(groupedM$Var1[groupedM$ont=="MF"] %in% groupedP$Var1)
+
+# View top 10
+
+View(head(groupedM, 10))
+View(head(groupedP, 10))
+
+# Compare top 10 ####
+
+getCountGO <- function(id, M, P){
+  message(id)
+  message("MEGAN")
+  idx <- which(M$Var1==id)
+  message("Position ", idx)
+  message(M$Freq[idx])
+  message("Protocol")
+  idx <- which(P$Var1==id)
+  message("Position ", idx)
+  message(P$Freq[idx])
+  message("\n")
+}
+
+for(i in groupedM[1:10, 1]){getCountGO(i, groupedM, groupedP)}
+
+for(i in groupedP[1:10, 1]){getCountGO(i, groupedM, groupedP)}
+
+# Top 10 filtered by ontology ####
+
+ontology <- "BP" # Change to "BP" for biological process, "CC" for cellular component, or "MF" for molecular function ####
+
+# Megan
+
+auxM <- groupedM[groupedM$ont==ontology,]
+auxM <- auxM[order(auxM$Freq, decreasing = T),]
+auxM <- auxM[1:10,]
+View(auxM)
+
+# Protocol
+
+auxP <- groupedP[groupedP$ont==ontology,]
+auxP <- auxP[order(auxP$Freq, decreasing = T),]
+auxP <- auxP[1:10,]
+View(auxP)
